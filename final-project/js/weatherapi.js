ae43b27c165da73fa9126eb69935a3c52bfa1ff7
ozonuweg <ozonuweglory@gmail.com>
2021-07-20 16:03:01 -0600

    final project

---
+++ final-project/js/weatherapi.js
@@ -0,0 +1,33 @@
+// WEATHER URL IN WARRI DELTA STATE WITH THE LATITUDE AND LOGITUDE INCLUDING MY API KEY
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=5.554399&lon=5.793201&appid=6aacbe3992a7c304c7c38a3f07ca053f";
+
+
+// CURRENT WEATHER SUMMARY
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        document.getElementById('condition').textContent = jsObject.current.weather[0].description;
+        document.getElementById('temperature').innerHTML = (jsObject.current.temp - 273.15).toFixed(2) + "&deg; C";
+        document.getElementById('feels-like').innerHTML = (jsObject.current.feels_like - 273.15).toFixed(2) + "&deg; C";
+        document.getElementById('humidity').textContent = jsObject.current.humidity + "%";
+    });
+
+
+// THREE DAY WEATHER FORECAST
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+
+     const weekday = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+     let day = 0;
+ 
+     // sort the daily forecast into the next three days aside the current day
+     const threedaysForecast = [ jsObject.daily[1], jsObject.daily[2], jsObject.daily[3] ];
+ 
+     threedaysForecast.forEach((x) => {
+       let d = new Date(x.dt * 1000);
+       document.getElementById(`day${day+1}`).textContent = weekday[d.getDay()];
+       document.getElementById(`temperature${day+1}`).textContent = x.temp.day;
+       day++;
+     });
+ });
